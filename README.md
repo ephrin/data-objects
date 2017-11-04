1d5769df26199b9f924fe9434f8d8ba9cf936d49
Vladimit Myrza <vmyrza@oroinc.com>
2017-11-04 10:58:28 +0200

    Documentation. Examples. - simple

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# data-objects
+# Dada Objects
+A library for simple objects .
+
+```php
+/**
+ * @property-read sring $data
+ */
+class DataPayload {
+    use Ephrin\Immutable\DocProperties;
+}
+
+$payload = new DataPayload();
+
+$payload->
+```
---
+++ examples/readOnly.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use Ephrin\Immutable\DocProperties;
+
+/**
+ * @property-read string $data
+ */
+class DataObject {
+    use DocProperties;
+}
+
+//the trait declares its own constructor with defaults as an assoc array
+$payload = new DataObject(['data' => 'Unchangeable property value :P']);
+
+//but you can use trait's factory method as well (to define own constructor)
+$payload = DataObject::fromArray(['data' => 'Unchangeable property value :P']);
+
+
+//static declaration is available trough `defaults` method.
+class
+
+
+//easy autocompletion of property with IDE
+print $payload->data . PHP_EOL; //prints content
+
+//IDE shows me that property isn't writable
+$payload->data = 'try'; //gets exception: Can not store value.... is not writable
+
+//undeclared properties are not allowed
+$payload->dynamic = 'hack you!!'; //gets exception: No such property ...
